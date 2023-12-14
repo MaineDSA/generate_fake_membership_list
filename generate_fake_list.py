29bdd7569560a9ece67cb4a2c1864d0a07bdca10
Wil Thieme <wil.thieme@protonmail.com>
2023-12-14 07:51:45 -0500

    allow loading zips from csv

--- generate_fake_list.py
+++ generate_fake_list.py
@@ -17,15 +17,16 @@ def main():
     parser = argparse.ArgumentParser(description="Member list generator")
     parser.add_argument("--chapter-name", help="Chapter name", default="Maine")
     parser.add_argument("--y-chapter-name", help="Youth chapter name", default="")
-    parser.add_argument(
-        "-n", help="Number of addresses to generate", default=10, type=int
+    parser.add_argument("-n", help="Number of addresses to generate", default=10, type=int)
+    parser.add_argument("--output", help="Output file name", default="fake_membership_list")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--chapter-addresses",
+        help="Generate real addresses for the specified chapter (see ./dsa_chapter_zip_codes/chapter_zips.csv)",
     )
-    parser.add_argument(
-        "--output", help="Output file name", default="fake_membership_list"
-    )
-    parser.add_argument(
-        "--real-address-zips",
-        help="Generate real addresses for these zip codes (comma-separated)",
+    group.add_argument(
+        "--zip-addresses",
+        help="Generate real addresses based on zip codes (comma-separated)",
     )
 
     args = parser.parse_args()
@@ -33,19 +34,29 @@ def main():
     # Access options using args.option1, args.option2, etc.
     print(args)
 
+    manual_zip_codes = []
+    if args.zip_addresses:
+        manual_zip_codes = list(args.zip_addresses.split(","))
+
+    chapter_zip_codes = []
+    if args.chapter_addresses and Path("./dsa_chapter_zip_codes/chapter_zips.csv"):
+        df = pd.read_csv("./dsa_chapter_zip_codes/chapter_zips.csv")
+        chapter_zip_codes = list(df.loc[df["chapter"] == args.chapter_addresses]["zip"])
+        chapter_zip_codes = [str(zip_code).zfill(5) for zip_code in chapter_zip_codes]
+
     people = []
     for _n in tqdm(range(args.n), unit="comrades"):
         person = generate_member()
-        if Path(".mapbox_token") and args.real_address_zips:
-            zip_code = random.choice(args.real_address_zips.split(","))
+        if not Path(".mapbox_token"):
+            person.update(get_fake_address())
+        elif manual_zip_codes or chapter_zip_codes:
+            zip_code = random.choice(manual_zip_codes or chapter_zip_codes)
             realistic_address = get_random_realistic_address(zip_code)
             if realistic_address:
                 person.update(realistic_address)
             else:
                 print("No realistic address found for zip code: ", zip_code, "...")
                 break
-        else:
-            person.update(get_fake_address())
         people.append(person)
 
     df = pd.DataFrame(data=people)
